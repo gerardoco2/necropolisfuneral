178b1e409a0200ca53ee1c047e6f0d3c27e478c2
Gerardo <gerardocolina@gmail.com>
2025-06-07 13:23:22 -0400

    intento de ejecutar script directamente

--- src/app/api/validarCedula/route.tsx
+++ src/app/api/validarCedula/route.tsx
@@ -23,7 +23,7 @@ export async function POST(request: NextRequest) {
 
   // Ejecuta el script en procesa
   const scriptPath = path.join(process.cwd(), "/ejec_pvx_verifica_cedula");
-  const command = `bash ${scriptPath} ${data.cedula}`;
+  const command = `bash ${scriptPath}`;
   console.log("Executing command:", command);
   await new Promise((resolve, reject) => {
     console.log("Executing dentro del promise:", command);
