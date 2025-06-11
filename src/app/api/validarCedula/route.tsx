d67a239d80fc1acf9bb0807dd31d3c13b916754f
Gerardo <gerardocolina@gmail.com>
2025-06-11 07:05:54 -0400

    refactor lectura del archivo para el login

--- src/app/api/validarCedula/route.tsx
+++ src/app/api/validarCedula/route.tsx
@@ -20,20 +20,10 @@ interface Beneficiario {
 }
 
 export async function POST(request: NextRequest) {
-  let afiliado: Afiliado;
-  // Parse the request body
   const data = await request.json();
-
-  // Define the content and file details
-
   let content = data.cedula;
-
-  //   for (let i = 0; i < data.beneficiario.length; i++) {
-  //     content += `\nBeneficiario ${i + 1}:\nNombre: ${data.beneficiario[i].nombre}\nApellido: ${data.beneficiario[i].apellido}\nCedula: ${data.beneficiario[i].cedula}\nFecha de Nacimiento: ${data.beneficiario[i].fechaNac}\nEdad: ${data.beneficiario[i].edad}\nParentesco: ${data.beneficiario[i].parentesco}`;
-  //   }
   const nombreArchivo = "CEDULA.TXT";
   const directory = "../archivos-procesa"; // guarda en "generated-files" directory
-  //const directory = "./generated-files"; // guarda en "generated-files" directory
 
   // Genera el archivo
   generaTxt(content, nombreArchivo, directory);
@@ -45,54 +35,55 @@ export async function POST(request: NextRequest) {
   );
   const command = `bash ${scriptPath}`;
   console.log("Executing command:", command);
-  await new Promise((resolve, reject) => {
+
+  const afiliado = await new Promise<Afiliado>((resolve, reject) => {
     console.log("Executing dentro del promise:", command);
     exec(command, (error, stdout, stderr) => {
       if (error) {
         console.error("Error executing script:", error);
         reject(error);
-      } else {
-        console.log("Script executed successfully:", stdout);
-        // leer el contenido del archivo generado
-        const filePath = path.join(directory, "SIONO.TXT");
-        const fs = require("fs");
-        fs.readFile(filePath, "utf8", (err: any, data: string) => {
-          if (err) {
-            console.error("Error reading file:", err);
-            reject(err);
-          } else {
-            console.log("File content:", data);
-            // Aquí puedes procesar el contenido del archivo como desees
-            // CONVIERTE EL CONTENIDO DEL ARCHIVO EN UN OBJETO JSON DE TIPO Afiliado
-            const lines = data.split("\n");
-            afiliado = {
-              cedula: lines[0].trim(),
-              nombre: lines[1].trim(),
-              estatus: lines[2].trim(),
-              nomina: lines[3].trim(),
-              beneficiario: [],
-            };
-            for (let i = 4; i < lines.length; i++) {
-              const parts = lines[i].split(";");
-              if (parts.length >= 4) {
-                const beneficiario: Beneficiario = {
-                  cedula: parts[0].trim(),
-                  nombre: parts[1].trim(),
-                  parentesco: parts[2].trim(),
-                  fechaNac: parts[3].trim(),
-                  edad: parts[4] ? parts[4].trim() : undefined,
-                };
-                afiliado.beneficiario?.push(beneficiario);
-              }
-            }
-            console.log("Afiliado object:", afiliado);
-            return NextResponse.json({ status: 200, afiliado: afiliado });
-            //resolve(stdout);
-          }
-        });
+        return;
       }
+      console.log("Script executed successfully:", stdout);
+      // leer el contenido del archivo generado
+      const filePath = path.join(directory, "SIONO.TXT");
+      const fs = require("fs");
+      fs.readFile(filePath, "utf8", (err: any, data: string) => {
+        if (err) {
+          console.error("Error reading file:", err);
+          reject(err);
+          return;
+        }
+        console.log("File content:", data);
+
+        // CONVIERTE EL CONTENIDO DEL ARCHIVO EN UN OBJETO JSON DE TIPO Afiliado
+        const lines = data.split("\n");
+        const adiliado: Afiliado = {
+          cedula: lines[0].trim(),
+          nombre: lines[1].trim(),
+          estatus: lines[2].trim(),
+          nomina: lines[3].trim(),
+          beneficiario: [],
+        };
+        for (let i = 4; i < lines.length; i++) {
+          const parts = lines[i].split(";");
+          if (parts.length >= 4) {
+            const beneficiario: Beneficiario = {
+              cedula: parts[0].trim(),
+              nombre: parts[1].trim(),
+              parentesco: parts[2].trim(),
+              fechaNac: parts[3].trim(),
+              edad: parts[4] ? parts[4].trim() : undefined,
+            };
+            afiliado.beneficiario?.push(beneficiario);
+          }
+        }
+        console.log("Afiliado object:", afiliado);
+        //return NextResponse.json({ status: 200, afiliado: afiliado });
+        resolve(afiliado);
+      });
     });
   });
   // restorna una respuesta JSON con el estado 200 y el objeto afiliado
-  return NextResponse.json({ status: 200 });
+  return NextResponse.json({ status: 200, afiliado });
 }
